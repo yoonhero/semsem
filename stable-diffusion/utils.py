694eebcdeae3fc8432f549ee7e7fc907d7b343f4
yoonhero <yoonhero06@naver.com>
2022-10-16 23:29:41 +0900

    feat: Data Preprocessing

---
+++ stable-diffusion/utils.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from regex import R
+import torch
+from torch import autocast
+
+# Convert many images to one grid image
+def makegrid(imgs, rows, cols):
+    assert len(imgs) == rows*cols
+
+    w, h = imgs[0].size
+    grid = Image.new("RGB", size=(cols*w, rows*h))
+
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i%cols*w, i//cols*h))
+    
+    return grid
+
+
+def get_device():
+    return "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def animalism(pipe, image_dir, prompt, strength, guidance_scale, device=get_device()):
+    init_img = Image.open(image_dir)
+    generator = torch.Generator(device=device)
+    with autocast("cuda"):
+        image = pipe(prompt=prompt, init_image=init_img, strength=strength, guidance_scale=guidance_scale, generator=generator).images[0]
+
+    return image
