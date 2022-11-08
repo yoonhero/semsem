5acb7bb9991c550beb4400aa5bf7bf9c962e7c72
yoonhero <yoonhero06@naver.com>
2022-11-09 08:00:11 +0900

    refactor: RestAPI

--- restapi/app/main.py
+++ restapi/app/main.py
@@ -1,122 +1,26 @@
 import base64
-import json
 import os
 from flask import Flask, request, jsonify, send_file
 from io import BytesIO
 from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
 from flask_cors import CORS, cross_origin
 import time
-from datetime import date
 
 from torch_utils import img2anime
 from imgur_uploader import Uploader
+from utils import serve_pil_image, make_image_frame, img_to_base64_str
 
 
 app = Flask(__name__)
 cors = CORS(app, resources={r'*': {'origins': '*'}})
-# app.config['CORS_HEADERS'] = 'Content-Type'
-# app.config["Access-Control-Allow-Origin"] = "*"
 
 ALLOWED_EXTENSIONS = {'png', "jpg", "jpeg"}
-
-# client = authenticate()
 
 imgur_uploader = Uploader()
 
 
 def allowed_file(filename):
     return '.' in filename and filename.split(".")[-1].lower() in ALLOWED_EXTENSIONS
-
-def get_today():
-    today = date.today()
-    return today.strftime("%y.%m.%d")
-
-
-def serve_pil_image(pil_img):
-    img_io = BytesIO()
-    pil_img.save(img_io, 'JPEG', quality=70)
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
-
-def image_grid(imgs, rows, cols):
-    assert len(imgs) == rows*cols
-
-    w, h = imgs[0].size
-    grid = Image.new('RGB', size=(cols*w, rows*h))
-    grid_w, grid_h = grid.size
-    
-    for i, img in enumerate(imgs):
-        grid.paste(img, box=(i%cols*w, i//cols*h))
-    return grid
-
-
-def resize_image_fit_frame(image, target_width):
-    w, h = image.size
-
-    new_height = int(target_width / w * h)
-
-    new_size = (target_width, new_height)
-
-    image = image.resize(new_size, resample=Image.Resampling.NEAREST)
-
-    crop_area = (0, new_height/2-365/2,618, new_height/2+365/2)
-
-    image = image.crop(crop_area)
-
-    return image
-
-
-def make_image_frame(imgs, frame):
-    text_pos = {
-        "frame_black":((618/2-30, 1600), (255, 255,255)),
-        "frame_blue":((618/2-30, 1680), (255, 255, 255)),
-        "frame_purple":((618/2-30, 1650), (255, 255, 255)),
-        "frame_white":((618/2-30, 1680), (0, 0, 0)),
-        "frame_green":((618/2-30, 1680), (0, 0, 0)),
-        "frame_red":((618/2-30, 1680), (255, 255, 255))
-    }
-
-    frames = ['./romela_frame.png','./frame_red.png','./frame_green.png','./frame_blue.png','./frame_purple.png','./frame_white.png','./frame_black.png',]
-
-    t_frame = frames[int(frame)]
-
-    if t_frame.split(".")[1][1:] != "romela_frame":
-        pos, rgb_color = text_pos.get(t_frame.split(".")[1][1:])
-
-
-    background = Image.open(t_frame)
-    w, original_h = background.size
-    # w, h = (618, 365)
-    h = original_h / 4.7
-
-    grid = Image.new("RGB", size=(w, original_h))
-
-    for i, img in enumerate(imgs):  
-        resized_img = resize_image_fit_frame(img, w)
-        grid.paste(resized_img, box=(0, int(i*h)))
-
-    
-    grid.paste(background, (0, 0), background)
-
-    if t_frame.split(".")[1][1:] != "romela_frame":
-        I1 = ImageDraw.Draw(grid)
-
-        myFont = ImageFont.truetype('Woojin_Hyun.ttf', 30)
-        t_text = get_today()
-        I1.text(pos,  t_text, font=myFont,  fill=rgb_color)
-
-    return grid
-
-
-def img_to_base64_str(img):
-    buffered = BytesIO()
-    img.save(buffered, format="PNG")
-    buffered.seek(0)
-    img_byte = buffered.getvalue()
-    img_str = "data:image/png;base64," + base64.b64encode(img_byte).decode()
-    return img_str
 
 
 @app.route("/api/predict", methods=["POST"])
@@ -129,18 +33,23 @@ def predict_for_web():
 
     results = []
 
+    # TODO Allowed File
+
     for image in images:
-        image = image[image.find(",") + 1 :]
-        dec = base64.b64decode(image + "===")
-        image = Image.open(BytesIO(dec))
+        try:
+            image = image[image.find(",") + 1 :]
+            dec = base64.b64decode(image + "===")
+            image = Image.open(BytesIO(dec))
 
-        print("Loading success")
+            print("Loading success")
 
-        anime_img = img2anime(image)
+            anime_img = img2anime(image)
 
-        print("processing success")
+            print("processing success")
 
-        results.append(anime_img)   
+            results.append(anime_img)   
+
+        except: return jsonify({"error": "Error when reading Image."})
 
 
     result_img = make_image_frame(results, frame)
---
+++ restapi/app/utils.py
@@ -0,0 +1,99 @@
+from flask import send_file
+import base64
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from datetime import date
+from io import BytesIO
+import json
+
+
+# 22.11.9
+def get_today():
+    today = date.today()
+    return today.strftime("%y.%m.%d")
+
+
+def serve_pil_image(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=80)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
+
+def make_grid(imgs, rows, cols):
+    assert len(imgs) == rows*cols
+
+    w, h = imgs[0].size
+    grid = Image.new('RGB', size=(cols*w, rows*h))
+    
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i%cols*w, i//cols*h))
+    return grid
+
+
+def resize_image_to_fit_frame(image, target_width):
+    w, h = image.size
+
+    new_height = int(target_width / w * h)
+
+    new_size = (target_width, new_height)
+
+    image = image.resize(new_size, resample=Image.Resampling.NEAREST)
+
+    crop_area = (0, new_height/2-365/2,618, new_height/2+365/2)
+
+    image = image.crop(crop_area)
+
+    return image
+
+
+def make_image_frame(imgs, frame):
+    text_pos = {
+        "frame_black":((618/2-30, 1600), (255, 255,255)),
+        "frame_blue":((618/2-30, 1680), (255, 255, 255)),
+        "frame_purple":((618/2-30, 1650), (255, 255, 255)),
+        "frame_white":((618/2-30, 1680), (0, 0, 0)),
+        "frame_green":((618/2-30, 1680), (0, 0, 0)),
+        "frame_red":((618/2-30, 1680), (255, 255, 255))
+    }
+
+    frames = ['./romela_frame.png','./frame_red.png','./frame_green.png','./frame_blue.png','./frame_purple.png','./frame_white.png','./frame_black.png',]
+
+    t_frame = frames[int(frame)]
+
+    background = Image.open(t_frame)
+    w, original_h = background.size
+    # w, h = (618, 365)
+    h = original_h / 4.7
+
+    grid = Image.new("RGB", size=(w, original_h))
+
+    for i, img in enumerate(imgs):  
+        resized_img = resize_image_to_fit_frame(img, w)
+        grid.paste(resized_img, box=(0, int(i*h)))
+
+    
+    grid.paste(background, (0, 0), background)
+
+    if t_frame.split(".")[1][1:] == "romela_frame":
+        return grid
+
+    pos, rgb_color = text_pos.get(t_frame.split(".")[1][1:])
+
+    I1 = ImageDraw.Draw(grid)
+
+    myFont = ImageFont.truetype('Woojin_Hyun.ttf', 30)
+    t_text = get_today()
+    I1.text(pos,  t_text, font=myFont,  fill=rgb_color)
+
+    return grid
+
+
+def img_to_base64_str(img):
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    buffered.seek(0)
+    img_byte = buffered.getvalue()
+    img_str = "data:image/png;base64," + base64.b64encode(img_byte).decode()
+    return img_str
